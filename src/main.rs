f1d35e1b6d5a0d14dbc9bbd83e690f8e3c73c965
anders martini <andersmartini2@gmail.com>
2018-04-10 12:19:37 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![feature(plugin)]
+#![feature(custom_derive)]
+#![plugin(rocket_codegen)]
+
+use rocket::request::FromForm;
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+
+#[derive(FromForm)]
+struct Topic{
+    url: String
+}
+
+#[get("/api/chat/messages?<topic>")]
+fn get_topic(topic: Topic) -> String {
+    format!("hello {} !", topic.url)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/", routes![get_topic])
+        .launch();
+}
