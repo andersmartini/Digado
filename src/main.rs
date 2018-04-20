8f20cd02375310b5a9a444601cca87c28612a1f5
Sam Wilson <sam.wilson@izettle.com>
2018-04-20 15:30:30 +0200

    Added some new endpoints, testing JSON using serde, had some fun learning about rocket

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,26 @@
 #![plugin(rocket_codegen)]
 
 use rocket::request::FromForm;
+use rocket::http::RawStr;
 
 extern crate rocket;
 
-#[get("/")]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+const CHAT: &'static [&'static str] = &["hello", "no u", "stop"];
+
+#[get("/world")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<name>")]
+fn hello(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
+}
 
 #[derive(FromForm)]
 struct Topic{
@@ -22,15 +34,26 @@ struct Subject{
     sam: String
 }
 
-#[get("/api/chat/messages?<topic>")]
+#[get("/chat/messages?<topic>")]
 fn get_topic(topic: Topic) -> String {
     format!("hello {} !", topic.url)
 }
 
+#[get("/chat/messages")]
+fn write_chat() -> String {
+    return serde_json::to_string(CHAT).unwrap();
+}
+
+#[post("/chat/message")]
+fn send_message() {
+
+}
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
-        .mount("/", routes![get_topic])
+        .mount("/hello", routes![index])
+        .mount("/hello", routes![hello])
+        .mount("/api", routes![get_topic])
+        .mount("/api", routes![write_chat])
         .launch();
 }
